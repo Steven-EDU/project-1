4348d0976195cb62587a18903f877d9a441d1111
Steven-EDU <gns.shou@gmail.com>
2026-09-16 11:29:24 -0500

    Base Upload

---
+++ main.py
@@ -0,0 +1,56 @@
+"""
+    First Project -- main.py
+
+    Steven, Salma, Ethil
+
+    
+"""
+#                C:\Users\necro\.local\bin\python3.14.exe "d:/Working/VS Code Project Files/Project_1/main.py"
+#Beginning Variables
+#Eventually swap to "dictionaries"
+is_Shopping = True
+snacks= ["Chips, Cookies, Pretzels"]
+food = ["Milk", "Bread", "Eggs"]
+lottery_ticket= ["$10 Ticket", "$100 Ticket"]
+
+
+
+
+
+
+
+
+drinks = ["water, Juice, Soda"]
+categories = ["Food", "Snacks", "Drinks", "Lottery Ticket"]
+
+
+
+#Code For Entering Shop
+# print("Welcome to Slow Way!")
+# print("What can i help you with? \n[1] Food"")
+# menu = ["[1] Food","[2] Snacks", "[3] Drinks," "[4] Lottery Ticket."]
+# user_Category = input("type here:")
+# input()
+
+#Code for Shopping
+
+
+
+# while is_Shopping:
+# """ The whole loop for shopping/decision making for the user """
+#     if var == 1
+#         # Food
+#     elif var == 2
+#         # Snacks
+#     elif var == 3
+#         # Drinks
+#         print("What type of drink would you like?")
+#         varTemp = input("[1] Drink")
+#     else:
+#         print("That's not an option. Try again:")
+
+#Code for Checkout
+# Should receive a list of items + how many items
+# Items to list
+# Number to get prices
+
