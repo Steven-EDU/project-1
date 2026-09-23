91266247087b7acac610692b400153a582685773
Steven Hou <gns.shou@gmail.com>
2026-09-22 19:44:56 -0500

    Fixed: - Shop Dictionary - Category logic - Prints items of the user_category
    Changed:
    - Few comments for formatting
    - Formatting for dictionary

--- main.py
+++ main.py
@@ -5,42 +5,57 @@
 
     
 """
-#                C:\Users\necro\.local\bin\python3.14.exe "d:/Working/VS Code Project Files/Project_1/main.py"
+# test
+#  C:\Users\necro\.local\bin\python3.14.exe "d:/Working/VS Code Project Files/Project_1/main.py"
 #Beginning Variables
 #Eventually swap to "dictionaries"
+
+#Setting Variables
+total: float = 0
+is_Shopping = True
+
+# Dictionary price
+Shop = {
+    "Snacks": {"Chips": 2.50, "Cookies": 3.20,"Pretzels": 2.48},
+    "Food":  {"Milk": 2.87,"Bread": 1.82,"Eggs": 3.89},
+    "Drinks": {"Water": 1.00, "Juice": 1.50, "Soda": 2.15},
+    "Lottery_Ticket": {"$10 Ticket": 10.0, "$100 Ticket": 100.0}
+}
+
 #Code For Entering Shop
 print("Welcome to Slow Way!")
 print("What can i help you with?\n")
+
+#Print Menu + Let User Choose
 menu = ["[1] Food","[2] Snacks", "[3] Drinks", "[4] Lottery Ticket."]
+
 for option in menu :
     print (option)
+
 user_category = input("type here:")
 
 
-total: float = 0
 
-
-is_Shopping = True
-# Dictionary price
-Shop = {"Chips": 2.50, "Cookies": 3.20,"Pretzels": 2.48,"Milk": 2.87,"Bread": 1.82,"Eggs": 3.89,"$10 Ticket": 10.0,"$100 Ticket": 100}
-Snacks= ["Chips", "Cookies", "Pretzels"]
-Food = ["Milk", "Bread", "Eggs"]
-Drinks = ["Water, Juice, Soda"]
-lottery_ticket= ["$10 Ticket", "$100 Ticket"]
-
+# Display Items
 if user_category == "1" or user_category == "Food":
-    print(Food)
+    chosen = Shop.get("Food")
 
 elif  user_category == "2" or user_category == "Snacks":
-    print(Snacks)
+    chosen = Shop.get("Snacks")
 
 elif  user_category == "3" or user_category == "Drinks":
-    print(Drinks)
+    chosen = Shop.get("Drinks")
 
 elif  user_category == "4" or user_category == "Lottery Ticket":
-    print(lottery_ticket)
+    chosen = Shop.get("Lottery Ticket")
 else:
     print("Try typing that again please!")
+
+
+# Display category items and prices
+for item in chosen:
+     print(f"${chosen[item]:.2f} for {item}")
+
 
 text : str = input()
 if text in Shop:
@@ -59,27 +74,7 @@ else:
 
 
 
-categories = ["Food", "Snacks", "Drinks", "Lottery Ticket"]
 
-
-
-
-#Code for Shopping
-
-
-
-# while is_Shopping:
-# """ The whole loop for shopping/decision making for the user """
-#     if var == 1
-#         # Food
-#     elif var == 2
-#         # Snacks
-#     elif var == 3
-#         # Drinks
-#         print("What type of drink would you like?")
-#         varTemp = input("[1] Drink")
-#     else:
-#         print("That's not an option. Try again:")
 
 #Code for Checkout
 # Should receive a list of items + how many items
